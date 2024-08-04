deb540658505aec052deb193cfe037c2ebc97c7b
Zensos <rockemaner@gmail.com>
2024-08-04 20:06:44 +0700

    hotfix: shuffle function

--- src/member/bingo/bingo.service.ts
+++ src/member/bingo/bingo.service.ts
@@ -72,11 +72,17 @@ export class BingoService {
   };
 
   private shuffleArray = (array: any) => {
-    for (let i = array.length - 1; i > 0; i--) {
+    const shuffledArray = array.slice();
+
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
     }
-    return array;
+
+    return shuffledArray;
   };
   private randomPrize(): string {
     const ranges = [];
