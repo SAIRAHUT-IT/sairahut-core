4f44b0eb470d36061ea6061bef59db11bfa2d4c5
Zensos <rockemaner@gmail.com>
2024-08-13 13:09:36 +0700

    update: protect

--- src/member/puzzle/puzzle.controller.ts
+++ src/member/puzzle/puzzle.controller.ts
@@ -2,7 +2,10 @@ import {
   Body,
   Controller,
   Get,
+  Header,
+  Headers,
   Patch,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { PuzzleService } from './puzzle.service';
@@ -11,13 +14,18 @@ import { ValidateMemberDto } from 'src/dtos/auth/auth.dto';
 import { JwtAuthGuard } from 'src/libs/auth/jwt-auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { PatchPuzzle } from 'src/dtos/puzzle/puzzle.dto';
+import { HttpService } from '@nestjs/axios';
+import { Response } from 'express';
 
 @ApiTags('PUZZLE')
 @Controller('puzzle')
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 export class PuzzleController {
-  constructor(private readonly puzzleService: PuzzleService) {}
+  constructor(
+    private readonly puzzleService: PuzzleService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Patch()
   patchPuzzle(
@@ -33,7 +41,27 @@ export class PuzzleController {
   }
 
   @Get('image')
-  getPuzzleImage(@MemberValidator() member: ValidateMemberDto) {
-    return this.puzzleService.getPuzzleImage(member);
+  async getPuzzleImage(
+    @Res() res: Response,
+    @MemberValidator() member: ValidateMemberDto,
+  ) {
+    try {
+      const imageUrl = await this.puzzleService.getPuzzleImage(
+        member,
+      );
+
+      const response = await this.httpService
+        .get(imageUrl, { responseType: 'arraybuffer' })
+        .toPromise();
+
+      const buffer = Buffer.from(response.data, 'binary');
+      const base64Image = buffer.toString('base64');
+      const mimeType = 'image/webp';
+
+      res.send({ image: `data:${mimeType};base64,${base64Image}` });
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      res.status(500).send('Failed to load image.');
+    }
   }
 }
--- src/member/puzzle/puzzle.module.ts
+++ src/member/puzzle/puzzle.module.ts
@@ -3,8 +3,10 @@ import { PuzzleService } from './puzzle.service';
 import { PuzzleController } from './puzzle.controller';
 import { PrismaService } from 'src/libs/prisma';
 import { JwtStrategy } from 'src/libs/auth/jwt.strategy';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [PuzzleController],
   providers: [PuzzleService, PrismaService, JwtStrategy],
 })
